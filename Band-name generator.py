b34909ace5e550515dcea0adb2db96a5ebfda840
kasi-adam <raahaadam8@gmail.com>
2025-07-15 09:31:31 +0000

    Add files via upload

---
+++ Band-name generator.py
@@ -0,0 +1,4 @@
+print("WELCOME TO PYTHON NAME GENERATOR")
+city_raised=input("what city did you grow up in?:")
+animal=input("what is your favorite animal?:")
+print("your band-name is " + city_raised + "-" + animal)
