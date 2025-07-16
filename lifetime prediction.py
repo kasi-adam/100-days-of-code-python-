1692bfda77c3b15d6327e92d2de6ca657d2324b1
kasi-adam <raahaadam8@gmail.com>
2025-07-16 01:35:20 +0000

    Add files via upload

---
+++ lifetime prediction.py
@@ -0,0 +1,7 @@
+print("welcome to my lifetime prediction!")
+age=int(input("your age:"))
+years_left=90-age
+months_left=12*years_left
+weeks_left=years_left*52
+days_left=years_left*365
+print(f"you have {days_left} days,{weeks_left} weeks,and {months_left} months left.")
